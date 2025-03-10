bccd551c225317d200e5d1834a5e864b4ca501a2
Ehsanullah Farooqi <ehsan.farooqi12@gmail.com>
2025-03-11 00:51:33 +0100

    Sorting Cabins

--- src/features/cabins/CabinTableOperation.jsx
+++ src/features/cabins/CabinTableOperation.jsx
@@ -1,5 +1,6 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperation() {
   return (
@@ -10,6 +11,16 @@ function CabinTableOperation() {
           { value: "all", Label: "All" },
           { value: "no-discount", Label: "No Discount" },
           { value: "with-discount", Label: "With Discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by name (A-Z)" },
+          { value: "name-desc", label: "Sort by name (Z-A)" },
+          { value: "regularPrice-asc", label: "Sort by price (low frist)" },
+          { value: "regularPrice-desc", label: "Sort by price (high frist)" },
+          { value: "maxCapacity-asc", label: "Sort by capacity (low frist)" },
+          { value: "maxCapacity-desc", label: "Sort by capacity (high frist)" },
         ]}
       />
     </TableOperations>
