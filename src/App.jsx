d19b0be1cd77ccc0a5695b9c5168cb02cf0071bf
Ehsanullah Farooqi <ehsan.farooqi12@gmail.com>
2025-03-13 02:25:58 +0100

    create booking detail pag

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
@@ -12,7 +13,7 @@ import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
-import { Toaster } from "react-hot-toast";
+import Booking from "./pages/Booking";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -36,6 +37,7 @@ function App() {
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="account" element={<Account />} />
             <Route path="bookings" element={<Bookings />} />
+            <Route path="bookings/:bookingId" element={<Booking />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="settings" element={<Settings />} />
             <Route path="users" element={<Users />} />
--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Empty from "../../ui/Empty";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
 import Pagination from "../../ui/Pagination";
+import Menus from "../../ui/Menus";
 
 function BookingTable() {
   const { isLoading, bookings, count } = useBookings();
@@ -12,25 +13,29 @@ function BookingTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
-      <Table.Header>
-        <div>Cabin</div>
-        <div>Guest</div>
-        <div>Dates</div>
-        <div>Status</div>
-        <div>Amount</div>
-        <div></div>
-      </Table.Header>
+    <Menus>
+      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
+        <Table.Header>
+          <div>Cabin</div>
+          <div>Guest</div>
+          <div>Dates</div>
+          <div>Status</div>
+          <div>Amount</div>
+          <div></div>
+        </Table.Header>
 
-      <Table.Body
-        data={bookings}
-        render={(booking) => <BookingRow key={booking.id} booking={booking} />}
-      />
+        <Table.Body
+          data={bookings}
+          render={(booking) => (
+            <BookingRow key={booking.id} booking={booking} />
+          )}
+        />
 
-      <Table.Footer>
-        <Pagination count={count} />
-      </Table.Footer>
-    </Table>
+        <Table.Footer>
+          <Pagination count={count} />
+        </Table.Footer>
+      </Table>
+    </Menus>
   );
 }
 
